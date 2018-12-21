53a7a9c50220c86e67d3d800ae0638fb48f81acc
heriwei <617443938@qq.com>
2018-12-21 18:25:16 +0800

    12-21初次提交MtReader

---
+++ readerlibrary/src/main/java/com/hrw/readerlibrary/MtReaderView.java
@@ -0,0 +1,28 @@
+package com.hrw.readerlibrary;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @version 1.0.0
+ * @author:hrw
+ * @date:2018/12/21 14:14
+ * @desc:
+ */
+public class MtReaderView extends View {
+    public MtReaderView(Context context) {
+        super(context, null);
+    }
+
+    public MtReaderView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs, 0);
+    }
+
+    public MtReaderView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    
+}
---
+++ readerlibrary/src/main/java/com/hrw/readerlibrary/ReaderAdapter.java
@@ -0,0 +1,11 @@
+package com.hrw.readerlibrary;
+
+/**
+ * @version 1.0.0
+ * @author:hrw
+ * @date:2018/12/21 14:59
+ * @desc:
+ */
+public class ReaderAdapter {
+    
+}
